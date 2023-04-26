ec927033458bcbec1609a78f3cd858f777940f6d
Moseemp <moseemp@gmail.com>
2023-04-26 18:29:47 +0530

    first commit by Moseem

---
+++ src/main/java/com/BikkadIT/SpringMvcFirstAp/controller/WelcomeController.java
@@ -0,0 +1,17 @@
+package com.BikkadIT.SpringMvcFirstAp.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class WelcomeController {
+
+	@GetMapping("/welcomeMessage")
+	public String welcome(Model model) {
+		String welMsg = "Welcome To BikkadIT";
+		
+		model.addAttribute("MSG", welMsg);
+		return "wel";
+	}
+}
